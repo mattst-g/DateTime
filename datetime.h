eeaf1830d07bfd0acc7b7ae1248a477f3df77450
Matt Saint-Gregory <mattsaintdev@gmail.com>
2021-12-30 17:23:19 +0000

    Created header method definitions

--- datetime.h
+++ datetime.h
@@ -23,6 +23,60 @@ class datetime
 
 	public:
 
+		datetime() {}
+
+		datetime(const &other) : value(other.value) {} // copy constructor
+
+		virtual ~datetime() {}
+
+		// copy assign =
+
+		// move assign =
+
+		// operator +
+
+		// operator -
+
+		// operator ++
+
+		// operator --
+
+		void now();		
+
+		std::string tostring();
+		void fromstring();
+
+		void setdatetime(int &year, int &month, int &day, int &hour, int &minute, int &second);
+		void setdate(int &year, int &month, int &day);
+		void settime(int &hour, int &minute, int &second);
+
+		int dayofweek();
+		int dayofyear();
+		int weekofyear();
+
+		void addseconds(int count = 1);
+		void addminutes(int count = 1);
+		void addhours(int count = 1);
+		void adddays(int count = 1);
+
+		void setfirstdayofmonth();
+		void setlastdayofmonth();
+
+		bool isleapyear();
+
+		bool samedatetime(const datetime &other);
+		bool samedate(const datetime &other);
+		bool sametime(const datetime &other);
+		bool samehour(const datetime &other);
+		bool sameminute(const datetime &other);
+		bool samesecond(const datetime &other);
+
+		int secondsapart(const datetime &other);
+		int minutesapart(const datetime &other);
+		int hoursapart(const datetime &other);
+		int daysapart(const datetime &other);
+
+		void difference(const datetime &other, int &days, int &hours, int &minutes, int &seconds);
 };
 
 #endif
